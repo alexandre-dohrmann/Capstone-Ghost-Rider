fa151b2c923ab7bbd066a5d0401c5c74ce4d48e8
Sergio Souza <emailsouza@gmail.com>
2018-08-30 17:18:41 -0600

    added welcome <user> to navbar, logout functionality

--- src/Login/Logout.js
+++ src/Login/Logout.js
@@ -1,48 +1,21 @@
 /* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, NavItem, NavLink, Form, FormGroup, Label, Input } from 'reactstrap';
-class Login extends React.Component {
-  constructor(props) {
+class Logout extends React.Component {
+  constructor() {
     super();
-    this.state = {
-      modal: false,
-    };
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle() {
-    this.setState({
-      modal: !this.state.modal
-    });
   }
 
   render() {
     return (
       <div>
         <NavItem>
-          <Button className="nav-links" onClick={this.toggle}>Logout</Button>
+          <form onSubmit={this.props.handleLogout}>
+            <Button className="nav-links" type="submit">Logout</Button>
+          </form>       
         </NavItem>
-        <Modal isOpen={this.state.modal} modalTransition={{ timeout: 400 }} backdropTransition={{ timeout: 700 }}
-          toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Logout</ModalHeader>
-          <ModalBody>
-            <Form onSubmit={this.props.handleSubmit} inline>
-              <FormGroup>
-                <Label for="exampleEmail" hidden>Email</Label>
-                <Input type="text" name="username" id="exampleEmail" onChange={this.props.handleChange} placeholder="Email (username)" />
-              </FormGroup>
-              {' '}
-              <FormGroup>
-                <Label for="examplePassword" hidden>Password</Label>
-                <Input type="password" name="password" id="examplePassword" onChange={this.props.handleChange} placeholder="Password" />
-              </FormGroup>
-              {' '}
-              <Button type="submit" onClick={this.toggle}>Login</Button>
-            </Form>
-          </ModalBody>
-        </Modal>
       </div>
     );
   }
 }
-export default Login;
+export default Logout;
